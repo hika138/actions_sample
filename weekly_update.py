8ead0a959f74402211c7b7d1f54f84ef782c5f9e
hika138 <46370237+hika138@users.noreply.github.com>
2025-03-19 19:11:05 +0900

    リリース情報取得処理に例外処理を追加し、終了メッセージを出力するように修正

--- weekly_update.py
+++ weekly_update.py
@@ -12,27 +12,33 @@ def main(token, default_repository):
         repositories = json.load(json_file)
 
     # 対象リポジトリのリリース情報を取得し、更新があればissueを作成、更新
-    for repository in repositories:
-        address = repositories[repository]['address']
-        output = []
-        
-        # リリース情報を取得
-        release_json = subprocess.run(f"gh release list --repo {address} --json createdAt,tagName", capture_output=True, text=True, shell=True).stdout
-        print(release_json)
-        release = json.loads(release_json)
-        # 7日以内のリリース情報を取得
-        for i in range(len(release)):
-            if datetime.datetime.strptime(release[i]['createdAt'], '%Y-%m-%dT%H:%M:%SZ') > datetime.datetime.now() - datetime.timedelta(days=7):
-                  output.append(release[i]['tagName'])
-        if len(output) > 0:
-            # issueを作成
-            issue_title = f"{repository}: {(datetime.datetime.now() - datetime.timedelta(days=7)).strftime('%Y-%m-%d')} ~ {datetime.datetime.now().strftime('%Y-%m-%d')}のリリース情報"
-            with open(f"{repository}.md", "w") as f:
-                f.write("以下のリリースがあります。\n")
-                for i in range(len(output)):
-                    f.write(f"[{output[i]}](https://github.com/nuxt/nuxt/releases/tag/{output[i]})\n")
-                
-            subprocess.run(f'gh issue create --title "{issue_title}" --repo {default_repository} --body-file {repository}.md', shell=True)
+    try: 
+        for repository in repositories:
+            address = repositories[repository]['address']
+            output = []
+            
+            # リリース情報を取得
+            release_json = subprocess.run(f"gh release list --repo {address} --json createdAt,tagName", capture_output=True, text=True, shell=True).stdout
+            print(release_json)
+            release = json.loads(release_json)
+            # 7日以内のリリース情報を取得
+            for i in range(len(release)):
+                if datetime.datetime.strptime(release[i]['createdAt'], '%Y-%m-%dT%H:%M:%SZ') > datetime.datetime.now() - datetime.timedelta(days=7):
+                    output.append(release[i]['tagName'])
+            if len(output) > 0:
+                # issueを作成
+                issue_title = f"{repository}: {(datetime.datetime.now() - datetime.timedelta(days=7)).strftime('%Y-%m-%d')} ~ {datetime.datetime.now().strftime('%Y-%m-%d')}のリリース情報"
+                with open(f"{repository}.md", "w") as f:
+                    f.write("以下のリリースがあります。\n")
+                    for i in range(len(output)):
+                        f.write(f"[{output[i]}](https://github.com/nuxt/nuxt/releases/tag/{output[i]})\n")
+                    
+                subprocess.run(f'gh issue create --title "{issue_title}" --repo {default_repository} --body-file {repository}.md', shell=True)
+    except Exception as e:
+        print(e)
+    
+    finally:
+        print("終了")
         
 if __name__=="__main__":
     token = sys.argv[1]
